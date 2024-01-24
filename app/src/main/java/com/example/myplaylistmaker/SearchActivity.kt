e070ecc4b945476440104ea44eec41d6371dbd9c
Danil <danil377377@proton.me>
2024-01-24 19:25:25 +0900

    Попытка 1

--- app/src/main/java/com/example/myplaylistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/myplaylistmaker/SearchActivity.kt
@@ -6,11 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.random.Random
 
 class SearchActivity : AppCompatActivity() {
@@ -18,6 +27,10 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var inputEditText: EditText
     private lateinit var recyclerView: RecyclerView
     private lateinit var tracksAdapter: TrackAdapter
+    private lateinit var retryButton:Button
+    private lateinit var nothingFound:LinearLayout
+    private lateinit var internetProblems:LinearLayout
+
 
     @SuppressLint("MissingInflatedId")
 
@@ -30,7 +43,7 @@ class SearchActivity : AppCompatActivity() {
 
 
         tracksAdapter = TrackAdapter(
-            getTrackList())
+            tracks)
 
         recyclerView.adapter = tracksAdapter
 
@@ -40,6 +53,9 @@ class SearchActivity : AppCompatActivity() {
 
         val backButton = findViewById<ImageView>(R.id.back_button)
 
+         retryButton = findViewById<Button>(R.id.retry)
+         nothingFound = findViewById<LinearLayout>(R.id.nothing_found)
+         internetProblems = findViewById<LinearLayout>(R.id.internet_problems)
         inputEditText = findViewById<EditText>(R.id.input_text)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
@@ -48,9 +64,22 @@ class SearchActivity : AppCompatActivity() {
         }
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            tracks.clear()
+            tracksAdapter.notifyDataSetChanged()
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+        }
+        retryButton.setOnClickListener {
+            search()
+        }
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                search()
+                true
+            }
+            false
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -96,4 +125,63 @@ class SearchActivity : AppCompatActivity() {
         const val INPUT = "input"
 
     }
+
+    private val ITunesBaseUrl ="https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(ITunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val ITunesService = retrofit.create(ITunesApi::class.java)
+    private val tracks = ArrayList<Track>()
+    private fun search(){
+        ITunesService.search(inputEditText.text.toString())
+            .enqueue(object : Callback<ITunesResponse> {
+                override fun onResponse(
+                    call: Call<ITunesResponse>,
+                    response: retrofit2.Response<ITunesResponse>
+                ) {
+                    when (response.code()){
+                        200 -> {
+                            if (response.body()?.results?.isNotEmpty() == true){
+                                Log.d("Deb", response.body()?.results.toString())
+                                recyclerView.setVisibility(View.VISIBLE)
+                                internetProblems.setVisibility(View.GONE)
+                                nothingFound.setVisibility(View.GONE)
+                                tracks.clear()
+                                tracks.addAll(response.body()?.results!!)
+                                tracksAdapter.notifyDataSetChanged()
+                                showMessage("","")
+                            } else {
+                                showMessage("Ничего не найдено","")
+                                recyclerView.setVisibility(View.GONE)
+                                nothingFound.setVisibility(View.VISIBLE)
+                                internetProblems.setVisibility(View.GONE)
+                                Log.d("Deb", response.body()?.results.toString())
+                            }
+                        }
+                        else -> {
+                            showMessage("Что-то пошло не так", response.code().toString())
+                            internetProblems.setVisibility(View.VISIBLE)
+                            recyclerView.setVisibility(View.GONE)
+                            nothingFound.setVisibility(View.GONE)
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<ITunesResponse>, t: Throwable) {
+                    showMessage("Что-то пошло не так", t.message.toString())
+                    internetProblems.setVisibility(View.VISIBLE)
+                    recyclerView.setVisibility(View.GONE)
+                    nothingFound.setVisibility(View.GONE)
+                }
+            })
+    }
+    private fun showMessage(text: String, additionalText: String) {
+        if (text.isNotEmpty()){
+                Toast.makeText(this,additionalText, Toast.LENGTH_LONG)
+                    .show()
+            }
+
+    }
+
 }
--- app/src/main/java/com/example/myplaylistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/myplaylistmaker/TrackViewHolder.kt
@@ -1,10 +1,13 @@
 package com.example.myplaylistmaker
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
@@ -12,11 +15,12 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val songName: TextView = itemView.findViewById(R.id.song_name)
     private val singerName: TextView = itemView.findViewById(R.id.singer_name)
 
+    @SuppressLint("SetTextI18n")
     fun bind(model: Track) {
         Glide.with(itemView).load(model.artworkUrl100).placeholder(R.drawable.placeholder).into(songIcon)
 
         songName.text = model.trackName
-        singerName.text = "${model.artistName} • ${model.trackTime}"
+        singerName.text = "${model.artistName} • ${SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis)}"
 
 
     }
