4f8a5ff1509d5307ffa367ce5c8100254300a993
Danil <danil377377@proton.me>
2024-07-29 16:05:28 +0900

    первая вёрстка экрана плейлист

---
+++ app/src/main/java/com/example/myplaylistmaker/media/ui/PlaylistFragment.kt
@@ -0,0 +1,9 @@
+package com.example.myplaylistmaker.media.ui
+
+import androidx.fragment.app.Fragment
+import com.example.myplaylistmaker.databinding.FragmentPlaylistBinding
+
+
+class PlaylistFragment: Fragment() {
+    private lateinit var binding: FragmentPlaylistBinding
+}
